fb4e0c0a62407883ac80b31afc5530291c57a5db
Amit Khemnar <amit.khemnar1999@gmail.com>
2025-03-13 02:15:23 +0900

    hide all api key

--- src/main/java/com/project/weather/CurrencyService.java
+++ src/main/java/com/project/weather/CurrencyService.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class CurrencyService {
 
-    private static final String API_KEY = "312005109aabcd1cb60dea1b";  // Replace with your API key
+    private static final String API_KEY = System.getenv("C-API-KEY");  // Replace with your API key
     private static final String API_URL = "https://v6.exchangerate-api.com/v6/" + API_KEY + "/latest/{currency}";
 
     private final RestTemplate restTemplate = new RestTemplate();
